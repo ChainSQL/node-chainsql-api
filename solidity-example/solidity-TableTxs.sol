11df4bc422d6de7889d52e3cac5759fdd11e99a4
luleigreat <luleigreat@163.com>
2019-01-18 17:19:14 +0800

    change usercase of solidity-TableTxs.sol

--- solidity-example/solidity-TableTxs.sol
+++ solidity-example/solidity-TableTxs.sol
@@ -6,14 +6,13 @@ contract DBTest {
     * @param raw eg: "[{\"field\":\"id\", \"type\" : \"int\", \"length\" : 11, \"PK\" : 1, \"NN\" : 1, \"UQ\" : 1}, { \"field\":\"account\", \"type\" : \"varchar\" }, { \"field\":\"age\", \"type\" : \"int\" }]"
     */
 	function create(string tableName, string raw) public{
-		bool ret = msg.sender.create(tableName, raw);
-		require(ret,"create table failed");
+		msg.sender.create(tableName, raw);
 	}
 	/*
 	* @param tableName eg: "test1"
 	*/
-	function drop(string tableName) public returns(bool) {
-	    return msg.sender.drop(tableName);
+	function drop(string tableName) public{
+	    msg.sender.drop(tableName);
 	}
 	
 	/*
@@ -21,16 +20,16 @@ contract DBTest {
 	* @param tableName eg: "test1"
 	* @param raw eg: "[{\"account\":\"zU42yDW3fzFjGWosdeVjVasyPsF4YHj224\", \"id\":0}, {\"account\":\"zU42yDW3fzFjGWosdeVjVasyPsF4YHj224\",   \"id\":1}, {\"account\":\"zU42yDW3fzFjGWosdeVjVasyPsF4YHj224\", \"id\":2}]"
 	*/
-	function insert(address owner, string tableName, string raw) public returns(bool) {
-	    return owner.insert(tableName, raw);
+	function insert(address owner, string tableName, string raw) public{
+	    owner.insert(tableName, raw);
 	}
 	
 	/*
 	* @param tableName eg: "test1"
 	* @param raw eg: "[{\"account\":\"zU42yDW3fzFjGWosdeVjVasyPsF4YHj224\", \"id\":0}, {\"account\":\"zU42yDW3fzFjGWosdeVjVasyPsF4YHj224\",   \"id\":1}, {\"account\":\"zU42yDW3fzFjGWosdeVjVasyPsF4YHj224\", \"id\":2}]"
 	*/
-	function insert(string tableName, string raw) public returns(bool) {
-	    return msg.sender.insert(tableName, raw);
+	function insert(string tableName, string raw) public {
+	    msg.sender.insert(tableName, raw);
 	}
 	
 	/*
@@ -38,16 +37,16 @@ contract DBTest {
 	* @param tableName "test1"
 	* @param raw eg: "{\"id\":1}"
 	*/
-	function deletex(address owner, string tableName, string raw) public returns(bool) {
-	    return owner.deletex(tableName, raw);
+	function deletex(address owner, string tableName, string raw) public {
+	    owner.deletex(tableName, raw);
 	}
 	
 	/*
 	* @param tableName "test1"
 	* @param raw eg: "{\"id\":1}"
 	*/
-	function deletex(string tableName, string raw) public returns(bool) {
-	    return msg.sender.deletex(tableName, raw);
+	function deletex(string tableName, string raw) public {
+	    msg.sender.deletex(tableName, raw);
 	}
 	
 	/*
@@ -56,8 +55,8 @@ contract DBTest {
 	* @param rawUpdate eg: "{\"age\":15}"
 	* @param rawGet eg: "{\"id\": 2}"
 	*/
-	function update(address owner, string tableName, string rawUpdate, string rawGet) public returns(bool) {
-	    return owner.update(tableName, rawUpdate, rawGet);
+	function update(address owner, string tableName, string rawUpdate, string rawGet) public{
+	    owner.update(tableName, rawUpdate, rawGet);
 	}
 	
 	/*
@@ -65,16 +64,16 @@ contract DBTest {
 	* @param rawUpdate eg: "{\"age\":15}"
 	* @param rawGet eg: "{\"id\": 2}"
 	*/
-	function update(string tableName, string rawUpdate, string rawGet) public returns(bool) {
-	    return msg.sender.update(tableName, rawUpdate, rawGet);
+	function update(string tableName, string rawUpdate, string rawGet) public{
+	    msg.sender.update(tableName, rawUpdate, rawGet);
 	}
 	
 	/*
 	* @param tableName eg: "test1"
 	* @param tableNameNew eg: "testNew1"
 	*/
-	function rename(string tableName, string tableNameNew) public returns(bool) {
-	    return msg.sender.rename(tableName, tableNameNew);
+	function rename(string tableName, string tableNameNew) public{
+	    msg.sender.rename(tableName, tableNameNew);
 	}
 	
 	/*
@@ -82,17 +81,17 @@ contract DBTest {
 	* @param tableName eg: "test1"
 	* @param raw eg: "{\"insert\":false, \"delete\":false}"
 	*/
-	function grant(address toWho, string tableName, string raw) public returns(bool) {
+	function grant(address toWho, string tableName, string raw) public{
 	    return msg.sender.grant(toWho, tableName, raw);
 	}
 	
-	function sqlTransaction(string tableName) public returns(bool) {
+	function sqlTransaction(string tableName) public{
 	    db.beginTrans();
 	    msg.sender.create(tableName, "[{\"field\":\"id\", \"type\" : \"int\", \"length\" : 11, \"PK\" : 1, \"NN\" : 1, \"UQ\" : 1}, { \"field\":\"account\", \"type\" : \"varchar\" }, { \"field\":\"age\", \"type\" : \"int\" }]");
         msg.sender.insert(tableName, "[{\"account\":\"zU42yDW3fzFjGWosdeVjVasyPsF4YHj224\", \"id\":1}, {\"account\":\"zU42yDW3fzFjGWosdeVjVasyPsF4YHj224\",   \"id\":2}]");
         msg.sender.deletex(tableName, "{\"id\":1}");
         msg.sender.update(tableName, "{\"account\":\"id==2\"}", "{\"id\": 2}");
-        return db.commit();
+        db.commit();
 	}
 
     /*
@@ -102,6 +101,7 @@ contract DBTest {
     */
     function get(address owner, string tableName, string raw) public view returns(string) {
         uint256 handle = owner.get(tableName, raw);
+		require(handle != uint256(0), "Get table data failed,maybe user not authorized!");
         uint row = db.getRowSize(handle);
         uint col = db.getColSize(handle);
         string memory xxx;
@@ -125,6 +125,7 @@ contract DBTest {
     */
     function get(address owner, string tableName, string raw, string field) public view returns(string) {
         uint256 handle = owner.get(tableName, raw);
+		require(handle != uint256(0), "Get table data failed,maybe user not authorized!");
         uint row = db.getRowSize(handle);
         string memory xxx;
         for(uint i=0; i<row; i++)
